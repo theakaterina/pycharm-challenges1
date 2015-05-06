2ab6c6ced5d6d50b9142f363685fea3dbbac1335
theakaterina <theakoutsoukis@gmail.com>
2015-05-06 21:41:26 +1000

    Planned tasks

--- lesson1/task1/tests.py
+++ lesson1/task1/tests.py
@@ -1,4 +1,4 @@
-from test_helper import run_common_tests, failed, passed, get_answer_placeholders
+from test_helper import run_common_tests, failed, passed, get_answer_placeholders, get_file_output
 
 
 def test_answer_placeholders():
@@ -13,5 +13,19 @@ def test_answer_placeholders():
 if __name__ == '__main__':
     run_common_tests()
     # test_answer_placeholders()       # TODO: uncomment test call
+    answers = get_file_output()
+    if answers[0] == str(range(1, 21)):
+        passed()
+    else:
+        failed("answer 1 wrong")
+    if answers[1] == str(range(0, 31, 2)):
+        passed()
+    else:
+        failed("answer 2 wrong")
+    if answers[2] == str(range(99, 0, -1)):
+        passed()
+    else:
+        failed("answer 3 wrong")
+
 
 
