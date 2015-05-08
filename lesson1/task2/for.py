c51ba9124a79f5bc93bd0bb554428a759958d311
Thea <theakoutsoukis@gmail.com>
2015-05-08 21:44:24 +1000

    Delete for.py

--- lesson1/task2/for.py
+++
@@ -1,8 +0,0 @@
-# Calculating factorials using for loops
-
-nine_factorial = 1  # What would happen if we initialised the variable inside the for loop?
-for i in range(1, 10):
-    nine_factorial *= i
-
-print nine_factorial
-
