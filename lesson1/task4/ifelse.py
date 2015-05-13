8c0188741e1bfc27f5afa45047889fddd3ad42c2
theakaterina <theakoutsoukis@gmail.com>
2015-05-13 12:08:29 +1000

    Cleaned up text for 99 bottles lesson

--- lesson1/task4/ifelse.py
+++ lesson1/task4/ifelse.py
@@ -2,5 +2,9 @@ bikes = 5
 people = 7
 
 if bikes == people:
-    print
+    print "Yes! Let's ride bikes"
 # Now you do the rest of the statements!
+elif bikes < people:
+    print "Damn. Not enough bikes"
+else:
+    print "Should we leave bikes behind? I can't decide."
